9a089617fcdf15ed609022aa6281881aa170143d
Unknown <jplopera86@outlook.com>
2018-09-14 14:26:12 -0500

    20180914PrimeraMitad

--- Proyecto3/index.php
+++ Proyecto3/index.php
@@ -7,12 +7,45 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>
+            Proyecto 3
+        </title>
     </head>
     <body>
         <h1>
             <?php
-                echo 'Hola mundo';
+                //Esto es un comentario
+                $PrimeraVariable = 2;
+                echo $PrimeraVariable . "<br>";
+                $SegundaVariable = 5;
+                echo $SegundaVariable . "<br>";
+                echo $PrimeraVariable + $SegundaVariable . "<br>";
+                echo $PrimeraVariable - $SegundaVariable . "<br>";
+                echo $PrimeraVariable * $SegundaVariable . "<br>";
+                echo $PrimeraVariable / $SegundaVariable . "<br>";
+                if($PrimeraVariable < 1 && $SegundaVariable > 2){
+                    echo "Soy menor" . "<br>";
+                }
+                else{
+                    echo "Soy menor" . "<br>";
+                }
+                $lenguaje = 'Javascript';
+                switch ($lenguaje){
+                    case "PHP":
+                        echo 'PHP'. "<br>";
+                        break;
+                    case "Javascript":
+                        echo 'Javascript'. "<br>";
+                        break;
+                    case "html":
+                        echo 'html'. "<br>";
+                        break;
+                }
+                //Arreglos
+                $LenguajeDos = ["PHP","Javascript","html"];
+                echo $LenguajeDos[0] . "<br>";
+                echo $LenguajeDos[1] . "<br>";
+                echo $LenguajeDos[2] . "<br>";
             ?>
         </h1>
     </body>
