5ecebc226b9a076124368e00a77a7262bffb00e2
jplopera86 <43217917+jplopera86@users.noreply.github.com>
2018-09-19 17:22:21 -0500

    Project4

---
+++ Project4/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="registro.php" method="get">
+            <label>
+                Nombre
+            </label>
+            <input type="text" name="Nombre" placeholder="Ingrese su nombre">
+            <label>
+                Apellido
+            </label>
+            <input type="text" name="Apellido" placeholder="Ingrese su apellido">
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
+</html>
---
+++ Project4/registro.php
@@ -0,0 +1,6 @@
+<?php
+    //Obtener datos por get
+    $usuario = $_GET;
+    var_dump($usuario);
+    // Para mostrar un solo dato sería echo ($usuario['nombre']);
+?>
