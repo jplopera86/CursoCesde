875bf5489d6ea3401b0929d4244843da8ad6518d
jplopera86 <43217917+jplopera86@users.noreply.github.com>
2018-09-26 16:24:43 -0500

    Clase20180926

---
+++ Gatitos/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Gatitos
+        </title>
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <section>
+            
+        </section>
+        <footer>
+            
+        </footer>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
