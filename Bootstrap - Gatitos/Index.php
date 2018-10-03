434bf1d5869e3ed5f01f95e2e85e4b627a5540a6
jplopera86 <43217917+jplopera86@users.noreply.github.com>
2018-10-03 10:44:53 -0500

    gatitos

---
+++ Bootstrap - Gatitos/Index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>
+            Gatitos
+        </title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/estilos.css">
+    </head>
+    <body>
+        <header>
+            <!-- barra de navegación con Image and text -->
+            <nav class="navbar navbar-light barra-nav">
+                <a class="navbar-brand" href="#">
+                    <img src="img/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+                    Gatitos
+                </a>
+            </nav>
+        </header>
+        <section>
+            
+        </section>
+        <?php
+            // put your code here
+        ?>
+        <footer>
+            <div class="container-fluid style-footer">
+                &copy derechos reservados
+            </div>
+        </footer>
+        <script src="js/jquery-3.3.1.min.js">
+            
+        </script>
+        <script src="js/popper.min.js">
+            
+        </script>
+        <script src="js/bootstrap.min.js">
+            
+        </script>
+    </body>
+</html>
